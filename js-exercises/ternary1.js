5db23a6d2e4e5cb45160f3ded56b830d0bceede8
bwiil-malloc <swillassey@gmail.com>
2024-03-12 13:37:53 +0100

    upload ternary operator exercises

---
+++ js-exercises/ternary1.js
@@ -0,0 +1,14 @@
+/*
+Rewrite this if using the conditional operator '?':
+
+let result;
+
+if (a + b < 4) {
+  result = 'Below';
+} else {
+  result = 'Over';
+}
+*/
+let result;
+
+(a + b < 4) ? result = 'Below' : result = 'Over';
